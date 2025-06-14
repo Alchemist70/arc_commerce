1132e9afc6f00398e2c0c95c0e3cdd530e1cbc77
Alchemist70 <herberzs70@gmail.com>
2025-06-14 16:37:30 +0530

    Deploy on Render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.ACT_APP_API_URL}>
     <App />
   </BrowserRouter>
 );

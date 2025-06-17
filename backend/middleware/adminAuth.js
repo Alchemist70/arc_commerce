745ce7e85e111f2d1fe34c8772c710eaa08e6b84
Alchemist70 <herberzs70@gmail.com>
2025-06-17 22:38:19 +0530

    Solve issues

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,5 +1,5 @@
 const adminAuth = (req, res, next) => {
-  if (!req.user.isAdmin) {
+  if (!(req.user.isAdmin || req.user.is_admin)) {
     return res
       .status(403)
       .json({ message: "Access denied. Admin privileges required." });
